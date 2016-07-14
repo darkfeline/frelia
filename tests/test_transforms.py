109aa9f2cbd922170567e9a031a70eca8ff698e3
Allen Li <darkfeline@felesatra.moe>
2016-07-13 22:26:58 -0700

    Rename transform module to transforms
    Since the module contains transform callables.

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -4,18 +4,18 @@ import jinja2
 import pytest
 
 import frelia.page
-import frelia.transform
+import frelia.transforms
 
 
 def test_transform_group():
     mock_func = mock.Mock()
-    transform = frelia.transform.TransformGroup([mock_func])
+    transform = frelia.transforms.TransformGroup([mock_func])
     transform([mock.sentinel.object])
     assert mock_func.mock_calls == [mock.call([mock.sentinel.object])]
 
 
 def test_render_jinja(env, template, document):
-    render = frelia.transform.RenderJinja(env)
+    render = frelia.transforms.RenderJinja(env)
     assert document.content != 'rendered template'
     render([document])
     assert document.content == 'rendered template'
@@ -24,7 +24,7 @@ def test_render_jinja(env, template, document):
 def test_document_page_transform(document):
     page = frelia.page.Page('foo', document)
     document_func = mock.Mock()
-    page_func = frelia.transform.DocumentPageTransform(document_func)
+    page_func = frelia.transforms.DocumentPageTransform(document_func)
     page_func([page])
     positional_args = document_func.call_args[0]
     assert list(positional_args[0]) == [document]
@@ -32,26 +32,26 @@ def test_document_page_transform(document):
 
 def test_rebase_page_path(page):
     page.path = 'root/blog/post'
-    transform = frelia.transform.RebasePagePath('root')
+    transform = frelia.transforms.RebasePagePath('root')
     transform([page])
     assert page.path == 'blog/post'
 
 
 def test_strip_page_extension_path_html(page):
     page.path = 'blog/post.html'
-    frelia.transform.strip_page_extension([page])
+    frelia.transforms.strip_page_extension([page])
     assert page.path == 'blog/post'
 
 
 def test_strip_page_extension_path_index_html(page):
     page.path = 'blog/index.html'
-    frelia.transform.strip_page_extension([page])
+    frelia.transforms.strip_page_extension([page])
     assert page.path == 'blog/index.html'
 
 
 def test_strip_page_extension_path_nonhtml(page):
     page.path = 'static/style.css'
-    frelia.transform.strip_page_extension([page])
+    frelia.transforms.strip_page_extension([page])
     assert page.path == 'static/style.css'
 
 
