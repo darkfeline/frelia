58ff0eaf46463f12ee78bbca6f352c7ccf92706b
Allen Li <darkfeline@felesatra.moe>
2016-09-04 19:49:19 -0700

    Refactor atom.py

--- frelia/atom.py
+++ frelia/atom.py
@@ -4,17 +4,12 @@ https://tools.ietf.org/html/rfc4287
 
 """
 
-import frelia.jinja
-
-
-def render(feed):
-    context = {'feed': feed}
-    env = frelia.jinja.Environment()
-    template = env.get_template('atom.xml')
-    return template.render(context)
+import xml.etree.ElementTree as ET
 
 
 class Feed:
+
+    """Atom feed."""
 
     def __init__(
             self,
@@ -33,16 +28,33 @@ class Feed:
         self.authors = authors
         self.entries = entries
 
+    def to_xml(self):
+        """Return ElementTree representation."""
+        entry = ET.Element('feed', xmlns='http://www.w3.org/2005/Atom')
+        ET.SubElement(entry, 'id').text = self.id
+        ET.SubElement(entry, 'title').text = self.title
+        ET.SubElement(entry, 'updated').text = self.updated.isoformat()
+        if self.rights is not None:
+            ET.SubElement(entry, 'rights').text = self.rights
+        entry.extend(link.to_xml() for link in self.links)
+        entry.extend(author.to_xml() for author in self.authors)
+        entry.extend(entry.to_xml() for entry in self.entries)
+        return entry
 
-class Author:
+    def render(self, file):
+        """Render Atom feed to a file.
 
-    def __init__(self, name, uri=None, email=None):
-        self.name = name
-        self.uri = uri
-        self.email = email
+        file should be in text mode.
+
+        """
+        element = self.to_xml()
+        document = ET.ElementTree(element)
+        document.write(file, encoding='unicode', xml_declaration=True)
 
 
 class Entry:
+
+    """Atom entry."""
 
     def __init__(
             self,
@@ -61,18 +73,85 @@ class Entry:
         self.links = links
         self.categories = categories
 
+    def to_xml(self):
+        """Return ElementTree representation."""
+        entry = ET.Element('entry')
+        ET.SubElement(entry, 'id').text = self.id
+        ET.SubElement(entry, 'title').text = self.title
+        ET.SubElement(entry, 'updated').text = self.updated.isoformat()
+        if self.summary is not None:
+            ET.SubElement(entry, 'summary', type='html').text = self.summary
+        if self.published is not None:
+            ET.SubElement(entry, 'published').text = self.published.isoformat()
+        entry.extend(link.to_xml() for link in self.links)
+        entry.extend(category.to_xml() for category in self.categories)
+        return entry
+
+
+class Person:
+
+    """Atom person construct.
+
+    Concrete subclasses must set the ELEMENT_TYPE class attribute.
+
+    """
+
+    def __init__(self, name, uri=None, email=None):
+        self.name = name
+        self.uri = uri
+        self.email = email
+
+    def to_xml(self):
+        """Return ElementTree representation."""
+        entry = ET.Element('author')
+        ET.SubElement(entry, 'name').text = self.name
+        if self.uri is not None:
+            ET.SubElement(entry, 'uri').text = self.uri
+        if self.email is not None:
+            ET.SubElement(entry, 'email').text = self.email
+        return entry
+
+
+class Author(Person):
+
+    """Atom author metadata."""
+
+    ELEMENT_TYPE = 'author'
+
 
 class Link:
+
+    """Atom link metadata."""
 
     def __init__(self, href, rel=None, type=None):
         self.href = href
         self.rel = rel
         self.type = type
 
+    def to_xml(self):
+        """Return ElementTree representation."""
+        element = ET.Element('link', href=self.href)
+        if self.rel is not None:
+            element.set('rel', self.rel)
+        if self.type is not None:
+            element.set('type', self.type)
+        return element
+
 
 class Category:
+
+    """Atom category metadata."""
 
     def __init__(self, term, scheme=None, label=None):
         self.term = term
         self.scheme = scheme
         self.label = label
+
+    def to_xml(self):
+        """Return ElementTree representation."""
+        element = ET.Element('category', term=self.term)
+        if self.scheme is not None:
+            element.set('scheme', self.scheme)
+        if self.label is not None:
+            element.set('label', self.label)
+        return element
--- frelia_tests/test_atom.py
+++ frelia_tests/test_atom.py
@@ -1,46 +1,118 @@
+import collections
 import datetime
+import io
 
 import pytest
 
 from frelia import atom
 
 
-@pytest.fixture
-def feed():
-    return atom.Feed(
-        id='frelia',
-        title='Frelia',
-        updated=datetime.datetime(2010, 1, 2),
-        authors=[atom.Author('darkfeline')],
-        entries=[
-            atom.Entry(
-                id='post',
-                title='Post',
-                updated=datetime.datetime(2010, 3, 4),
-                links=[atom.Link('http://localhost/post')],
-                categories=[atom.Category('news')])
-        ])
+@pytest.mark.parametrize(
+    ('id', 'title', 'updated'),
+    [('http://example.com/', 'Takumi Times', datetime.datetime(2016, 1, 8))])
+def test_render(id, title, updated):
+    """Test Feed.render()."""
+    feed = atom.Feed(id, title, updated)
+    file = io.StringIO()
+    feed.render(file)
+    assert file.getvalue() == '''\
+<?xml version='1.0' encoding='UTF-8'?>
+<feed xmlns="http://www.w3.org/2005/Atom"><id>http://example.com/</id>\
+<title>Takumi Times</title><updated>2016-01-08T00:00:00</updated></feed>'''
 
 
-def test_render(feed):
-    got = atom.render(feed)
-    assert got == """\
-<?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
+_FEED_PARAMS = ('id', 'title', 'updated', 'rights', 'links', 'authors',
+                'entries')
+_FeedArgs = collections.namedtuple('_FeedArgs', _FEED_PARAMS)
 
-  <id>frelia</id>
-  <title>Frelia</title>
-  <updated>2010-01-02T00:00:00</updated>
 
-  <author>
-    <name>darkfeline</name>
-  </author>
+@pytest.mark.parametrize(
+    _FEED_PARAMS,
+    [_FeedArgs(
+        id='http://example.com/',
+        title='Takumi Times',
+        updated=datetime.datetime(2016, 1, 8),
+        rights='Public Domain',
+        links=(),
+        authors=(),
+        entries=())])
+def test_feed(id, title, updated, rights, links, authors, entries):
+    """Test Feed.to_xml()."""
+    feed = atom.Feed(id, title, updated, rights, links, authors, entries)
+    element = feed.to_xml()
+    assert element.tag == 'feed'
+    assert element.find('id').text == id
+    assert element.find('title').text == title
+    assert element.find('updated').text == updated.isoformat()
+    assert element.find('rights').text == rights
 
-  <entry>
-    <id>post</id>
-    <title>Post</title>
-    <updated>2010-03-04T00:00:00</updated>
-    <link href="http://localhost/post" />
-    <category term="news" />
-  </entry>
-</feed>"""
+
+_ENTRY_PARAMS = ('id', 'title', 'updated', 'summary', 'published', 'links',
+                 'categories')
+_EntryArgs = collections.namedtuple('_EntryArgs', _ENTRY_PARAMS)
+
+
+@pytest.mark.parametrize(
+    _ENTRY_PARAMS,
+    [_EntryArgs(
+        id='http://example.com/dork-meets-robot',
+        title='Dork meets robot',
+        updated=datetime.datetime(2016, 1, 8),
+        summary='Girl meets girl daya',
+        published=datetime.datetime(2012, 10, 10),
+        links=(),
+        categories=())])
+def test_entry(id, title, updated, summary, published, links, categories):
+    """Test Entry.to_xml()."""
+    entry = atom.Entry(id, title, updated, summary, published, links,
+                       categories)
+    element = entry.to_xml()
+    assert element.tag == 'entry'
+    assert element.find('id').text == id
+    assert element.find('title').text == title
+    assert element.find('updated').text == updated.isoformat()
+    assert element.find('summary').text == summary
+    assert element.find('published').text == published.isoformat()
+
+
+@pytest.mark.parametrize(
+    ('name', 'uri', 'email'),
+    [('Nene', 'http://example.com/', 'dork@example.com')])
+def test_author(name, uri, email):
+    """Test Author.to_xml()."""
+    author = atom.Author(name, uri, email)
+    element = author.to_xml()
+    assert element.tag == 'author'
+    assert element.find('name').text == name
+    assert element.find('uri').text == uri
+    assert element.find('email').text == email
+
+
+@pytest.mark.parametrize(
+    ('term', 'scheme', 'label'),
+    [('dork', 'http://example.com/', 'Nene')])
+def test_category(term, scheme, label):
+    """Test Category.to_xml()."""
+    category = atom.Category(term, scheme, label)
+    element = category.to_xml()
+    assert element.tag == 'category'
+    assert element.attrib == {
+        'term': term,
+        'scheme': scheme,
+        'label': label,
+    }
+
+
+@pytest.mark.parametrize(
+    ('href', 'rel', 'type'),
+    [('http://example.com/', 'alternate', 'text/html')])
+def test_link(href, rel, type):
+    """Test Link.to_xml()."""
+    link = atom.Link(href, rel, type)
+    element = link.to_xml()
+    assert element.tag == 'link'
+    assert element.attrib == {
+        'href': href,
+        'rel': rel,
+        'type': type,
+    }
