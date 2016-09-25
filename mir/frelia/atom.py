2119cfc39663fbf1f889b29d6334af73d8f97e93
Allen Li <darkfeline@felesatra.moe>
2016-09-25 03:56:17 -0700

    Refactor atom

--- mir/frelia/atom.py
+++ mir/frelia/atom.py
@@ -1,194 +1,193 @@
-"""Atom feeds.
+"""Atom feed generator
+
+This module contains classes representing RFC 4287 Atom documents and elements.
+The classes have required and optional attributes corresponding to the elements
+in the RFC.  Elements have a method, to_xml(), that returns an ElementTree
+representation of themselves.  Documents can be rendered to files using
+render().
+
+Currently, not all Atom documents and elements are implemented.
+
+Classes:
+
+Feed -- Atom Feed Document
+Entry
 
 https://tools.ietf.org/html/rfc4287
 """
 
-import abc
+import datetime
+import functools
+import io
+import typing
 import xml.etree.ElementTree as ET
 
 from mir.monads import maybe
 
 
-def _make_subelement(element, tag, text):
-    """Create subelement."""
-    subelement = ET.SubElement(element, tag)
-    subelement.text = text
+class Document:
+
+    """Atom Document.
+
+    Documents are created with a root element and support rendering to a file.
+    """
+
+    def __init__(self, root_element: ET.Element):
+        self.root = root_element
+
+    def render(self, file: io.TextIOBase):
+        """Render document to the file."""
+        document = ET.ElementTree(self.root)
+        document.write(file, encoding='unicode', xml_declaration=True)
+
+
+def Feed(id, title, updated, rights=None, links=(), authors=(), entries=()):
+    """Atom Feed element constructor."""
+    element = ET.Element('feed')
+    element.set('xmlns', 'http://www.w3.org/2005/Atom')
+    _TextSubElement(element, ID, id)
+    _TextSubElement(element, Title, title)
+    _TextSubElement(element, Updated, updated)
+    _MaybeTextSubElement(element, Rights, rights)
+    element.extend(links)
+    element.extend(authors)
+    element.extend(entries)
+    return element
+
+
+def Entry(id, title, updated, summary=None, published=None, links=(),
+          categories=()):
+    """Atom Entry element constructor."""
+    element = ET.Element('entry')
+    _TextSubElement(element, ID, id)
+    _TextSubElement(element, Title, title)
+    _TextSubElement(element, Updated, updated)
+    _MaybeTextSubElement(element, Summary, summary)
+    _MaybeTextSubElement(element, Published, published)
+    element.extend(links)
+    element.extend(categories)
+    return element
+
+
+def _init_person(self, name, uri=None, email=None):
+    """Atom Person construct constructor.
+
+    This function is a detached method.
+    """
+    _TextSubElement(self, Name, name)
+    _MaybeTextSubElement(self, URI, uri)
+    _MaybeTextSubElement(self, Email, email)
+
+
+def Author(name, uri=None, email=None):
+    """Atom Author metadata element constructor."""
+    element = ET.Element('author')
+    _init_person(element, name, uri, email)
+    return element
+
+
+def Link(href, rel=None, type=None):
+    """Atom Link metadata element constructor."""
+    element = ET.Element('link')
+    element.set('href', href)
+    _maybe_set(element, 'rel', rel)
+    _maybe_set(element, 'type', type)
+    return element
+
+
+def Category(term, scheme=None, label=None):
+    """Atom Category metadata element constructor."""
+    element = ET.Element('category')
+    element.set('term', term)
+    _maybe_set(element, 'scheme', scheme)
+    _maybe_set(element, 'label', label)
+    return element
+
+
+def _maybe_set(self: ET.Element, key: str, value: typing.Optional[str]):
+    """Set the attribute key to value if value isn't None.
+
+    This function is a detached method.
+    """
+    if value is not None:
+        self.set(key, value)
+
+
+def _TextElement(tag: str, text_construct: 'TextConstruct'):
+    """Text Element constructor.
+
+    This represents an Atom Element that contains only a Text Construct.
+    """
+    element = ET.Element(tag)
+    element.text = str(text_construct)
+    text_construct.set_type(element)
+    return element
+
+
+class TextConstruct:
+
+    """Text Construct.
+
+    Represents an Atom Text Construct, which is basically a string with an
+    optional type.
+    """
+
+    def __init__(self, text='', type=None):
+        self._text = text
+        self._type = type
+
+    def __repr__(self):
+        return '{cls}({object!r}, type={type!r})'.format(
+            cls=type(self).__qualname__,
+            object=self._text,
+            type=self._type)
+
+    def __str__(self):
+        return self._text
+
+    def set_type(self, element):
+        """Set the type attribute of the element."""
+        if self._type is not None:
+            element.set('type', self._type)
+
+
+ID = functools.partial(_TextElement, 'id')
+Title = functools.partial(_TextElement, 'title')
+Rights = functools.partial(_TextElement, 'rights')
+Name = functools.partial(_TextElement, 'name')
+Summary = functools.partial(_TextElement, 'summary')
+URI = functools.partial(_TextElement, 'uri')
+Email = functools.partial(_TextElement, 'email')
+
+
+def _DataElement(tag: str, dt: datetime.date):
+    """Construct an Atom Element that contains only a Date Construct."""
+    assert isinstance(dt, datetime.date)
+    return _TextElement(tag, (TextConstruct(dt.isoformat())))
+
+
+Published = functools.partial(_DataElement, 'published')
+Updated = functools.partial(_DataElement, 'updated')
+
+
+def _TextSubElement(element: ET.Element, sub_type, text):
+    """Create a subelement with given text value.
+
+    If text is a string, it will be coerced into a TextConstruct.
+    """
+    if isinstance(text, str):
+        text = TextConstruct(text)
+    subelement = sub_type(text)
+    element.append(subelement)
     return subelement
 
 
-def _make_subelement_if_not_none(element, tag, text):
-    """Create a subelement if text is not None.
+def _MaybeTextSubElement(element: ET.Element, sub_type, text):
+    """Create a subelement with given text value if text is not None.
 
     Returns a Maybe monad.
     """
     if text is None:
         return maybe.Nothing()
     else:
-        return maybe.Just(_make_subelement(element, tag, text))
-
-
-def _extend_as_xml(element, constructs):
-    """Extend element with constructs converted to XML elements."""
-    element.extend(construct.to_xml() for construct in constructs)
-
-
-class Feed:
-
-    """Atom feed."""
-
-    def __init__(
-            self,
-            id,
-            title,
-            updated,
-            rights=None,
-            links=(),
-            authors=(),
-            entries=()):
-        self.id = id
-        self.title = title
-        self.updated = updated
-        self.rights = rights
-        self.links = links
-        self.authors = authors
-        self.entries = entries
-
-    def to_xml(self):
-        """Return ElementTree representation."""
-        entry = ET.Element('feed', xmlns='http://www.w3.org/2005/Atom')
-        _make_subelement(entry, 'id', self.id)
-        _make_subelement(entry, 'title', self.title)
-        _make_subelement(entry, 'updated', self.updated.isoformat())
-        _make_subelement_if_not_none(entry, 'rights', self.rights)
-        _extend_as_xml(entry, self.links)
-        _extend_as_xml(entry, self.authors)
-        _extend_as_xml(entry, self.entries)
-        return entry
-
-    def render(self, file):
-        """Render Atom feed to a file.
-
-        file should be in text mode.
-        """
-        element = self.to_xml()
-        document = ET.ElementTree(element)
-        document.write(file, encoding='unicode', xml_declaration=True)
-
-
-class Entry:
-
-    """Atom entry."""
-
-    def __init__(
-            self,
-            id,
-            title,
-            updated,
-            summary=None,
-            published=None,
-            links=(),
-            categories=()):
-        self.id = id
-        self.title = title
-        self.updated = updated
-        self.summary = summary
-        self.published = published
-        self.links = links
-        self.categories = categories
-
-    def to_xml(self):
-        """Return ElementTree representation."""
-        entry = ET.Element('entry')
-        _make_subelement(entry, 'id', self.id)
-        _make_subelement(entry, 'title', self.title)
-        _make_subelement(entry, 'updated', self.updated.isoformat())
-        summary = _make_subelement_if_not_none(entry, 'summary', self.summary)
-        summary.bind(_set_html_type)
-        _make_subelement_if_not_none(entry, 'published',
-                                     self.published.isoformat())
-        _extend_as_xml(entry, self.links)
-        _extend_as_xml(entry, self.categories)
-        return entry
-
-
-@maybe.monadic
-def _set_html_type(element):
-    """Set an Atom element to have HTML type content."""
-    element.set('type', 'html')
-    return element
-
-
-class Person:
-
-    """Atom person construct."""
-
-    def __init__(self, name, uri=None, email=None):
-        self.name = name
-        self.uri = uri
-        self.email = email
-
-    @property
-    @abc.abstractmethod
-    def ELEMENT_TYPE(self):
-        raise NotImplementedError
-
-    def add_to(self, element):
-        """Add person construct to element."""
-        _make_subelement(element, 'name', self.name)
-        _make_subelement_if_not_none(element, 'uri', self.uri)
-        _make_subelement_if_not_none(element, 'email', self.email)
-
-
-class Author:
-    """Atom author metadata."""
-
-    def __init__(self, name, uri=None, email=None):
-        self.person = Person(name=name,
-                             uri=uri,
-                             email=email)
-
-    def to_xml(self):
-        """Return ElementTree representation."""
-        entry = ET.Element('author')
-        self.person.add_to(entry)
-        return entry
-
-
-def _set_attr_if_not_none(element, attr, value):
-    """Set element attribute if value isn't None."""
-    if value is not None:
-        element.set(attr, value)
-
-
-class Link:
-
-    """Atom link metadata."""
-
-    def __init__(self, href, rel=None, type=None):
-        self.href = href
-        self.rel = rel
-        self.type = type
-
-    def to_xml(self):
-        """Return ElementTree representation."""
-        element = ET.Element('link', href=self.href)
-        _set_attr_if_not_none(element, 'rel', self.rel)
-        _set_attr_if_not_none(element, 'type', self.type)
-        return element
-
-
-class Category:
-
-    """Atom category metadata."""
-
-    def __init__(self, term, scheme=None, label=None):
-        self.term = term
-        self.scheme = scheme
-        self.label = label
-
-    def to_xml(self):
-        """Return ElementTree representation."""
-        element = ET.Element('category', term=self.term)
-        _set_attr_if_not_none(element, 'scheme', self.scheme)
-        _set_attr_if_not_none(element, 'label', self.label)
-        return element
+        return maybe.Just(_TextSubElement(element, sub_type, text))
--- tests/test_atom.py
+++ tests/test_atom.py
@@ -9,12 +9,14 @@ from mir.frelia import atom
 
 @pytest.mark.parametrize(
     ('id', 'title', 'updated'),
-    [('http://example.com/', 'Takumi Times', datetime.datetime(2016, 1, 8))])
+    [('http://example.com/', 'Takumi Times', datetime.datetime(2016, 1, 8))]
+)
 def test_render(id, title, updated):
-    """Test Feed.render()."""
+    """Test document rendering."""
     feed = atom.Feed(id, title, updated)
+    document = atom.Document(feed)
     file = io.StringIO()
-    feed.render(file)
+    document.render(file)
     assert file.getvalue() == (
         "<?xml version='1.0' encoding='UTF-8'?>\n"
         '<feed xmlns="http://www.w3.org/2005/Atom">'
@@ -25,12 +27,12 @@ def test_render(id, title, updated):
 
 _FEED_PARAMS = ('id', 'title', 'updated', 'rights', 'links', 'authors',
                 'entries')
-_FeedArgs = collections.namedtuple('_FeedArgs', _FEED_PARAMS)
+_FeedTest = collections.namedtuple('_FeedTest', _FEED_PARAMS)
 
 
 @pytest.mark.parametrize(
     _FEED_PARAMS,
-    [_FeedArgs(
+    [_FeedTest(
         id='http://example.com/',
         title='Takumi Times',
         updated=datetime.datetime(2016, 1, 8),
@@ -40,8 +42,7 @@ _FeedArgs = collections.namedtuple('_FeedArgs', _FEED_PARAMS)
         entries=())])
 def test_feed(id, title, updated, rights, links, authors, entries):
     """Test Feed.to_xml()."""
-    feed = atom.Feed(id, title, updated, rights, links, authors, entries)
-    element = feed.to_xml()
+    element = atom.Feed(id, title, updated, rights, links, authors, entries)
     assert element.tag == 'feed'
     assert element.find('id').text == id
     assert element.find('title').text == title
@@ -49,31 +50,39 @@ def test_feed(id, title, updated, rights, links, authors, entries):
     assert element.find('rights').text == rights
 
 
-_ENTRY_PARAMS = ('id', 'title', 'updated', 'summary', 'published', 'links',
-                 'categories')
-_EntryArgs = collections.namedtuple('_EntryArgs', _ENTRY_PARAMS)
+_ENTRY_PARAMS = ('id', 'title', 'updated', 'summary', 'summary_type',
+                 'published', 'links', 'categories')
+_EntryTest = collections.namedtuple('_EntryTest', _ENTRY_PARAMS)
 
 
 @pytest.mark.parametrize(
     _ENTRY_PARAMS,
-    [_EntryArgs(
-        id='http://example.com/dork-meets-robot',
-        title='Dork meets robot',
+    [_EntryTest(
+        id='http://example.com/pandora',
+        title='Pandora',
         updated=datetime.datetime(2016, 1, 8),
-        summary='Girl meets girl daya',
+        summary='Girl meets girl',
+        summary_type='html',
         published=datetime.datetime(2012, 10, 10),
         links=(),
         categories=())])
-def test_entry(id, title, updated, summary, published, links, categories):
+def test_entry(id, title, updated, summary, summary_type, published, links,
+               categories):
     """Test Entry.to_xml()."""
-    entry = atom.Entry(id, title, updated, summary, published, links,
-                       categories)
-    element = entry.to_xml()
+    element = atom.Entry(
+        id=id,
+        title=title,
+        updated=updated,
+        summary=atom.TextConstruct(summary, type=summary_type),
+        published=published,
+        links=links,
+        categories=categories)
     assert element.tag == 'entry'
     assert element.find('id').text == id
     assert element.find('title').text == title
     assert element.find('updated').text == updated.isoformat()
     assert element.find('summary').text == summary
+    assert element.find('summary').get('type') == summary_type
     assert element.find('published').text == published.isoformat()
 
 
@@ -82,39 +91,33 @@ def test_entry(id, title, updated, summary, published, links, categories):
     [('Nene', 'http://example.com/', 'dork@example.com')])
 def test_author(name, uri, email):
     """Test Author.to_xml()."""
-    author = atom.Author(name, uri, email)
-    element = author.to_xml()
+    element = atom.Author(name, uri, email)
     assert element.tag == 'author'
     assert element.find('name').text == name
     assert element.find('uri').text == uri
     assert element.find('email').text == email
 
 
-@pytest.mark.parametrize(
-    ('term', 'scheme', 'label'),
-    [('dork', 'http://example.com/', 'Nene')])
+@pytest.mark.parametrize('term,scheme,label', [
+    ('dork', 'http://example.com/', 'Nene'),
+    ('dork', 'http://example.com/', None),
+])
 def test_category(term, scheme, label):
     """Test Category.to_xml()."""
-    category = atom.Category(term, scheme, label)
-    element = category.to_xml()
+    element = atom.Category(term, scheme, label)
     assert element.tag == 'category'
-    assert element.attrib == {
-        'term': term,
-        'scheme': scheme,
-        'label': label,
-    }
+    assert element.get('term', None) == term
+    assert element.get('scheme', None) == scheme
+    assert element.get('label', None) == label
 
 
 @pytest.mark.parametrize(
     ('href', 'rel', 'type'),
     [('http://example.com/', 'alternate', 'text/html')])
 def test_link(href, rel, type):
-    """Test Link.to_xml()."""
-    link = atom.Link(href, rel, type)
-    element = link.to_xml()
+    """Test Link."""
+    element = atom.Link(href, rel, type)
     assert element.tag == 'link'
-    assert element.attrib == {
-        'href': href,
-        'rel': rel,
-        'type': type,
-    }
+    assert element.get('href', None) == href
+    assert element.get('rel', None) == rel
+    assert element.get('type', None) == type
