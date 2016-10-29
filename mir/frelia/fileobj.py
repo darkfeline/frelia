32b5b1f9b7ac72a8a70b69390fa2a1736f2673d4
Allen Li <darkfeline@felesatra.moe>
2016-10-28 22:08:45 -0700

    Add fileobj

---
+++ mir/frelia/fileobj.py
@@ -0,0 +1,19 @@
+import collections
+
+import mir.frelia.fs
+
+
+FileObject = collections.namedtuple('FileObject', 'path,object')
+
+
+class FileObjectLoader:
+
+    def __init__(self, loader):
+        self._loader = loader
+
+    def __call__(self, rootdir):
+        loader = self._loader
+        for filepath in mir.frelia.fs.find_files(rootdir):
+            with open(filepath) as file:
+                object_ = loader(file)
+            yield FileObject(filepath, object_)
