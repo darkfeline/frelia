1dd3a73ece81a2710418cac411b1224a15fc60b1
Allen Li <darkfeline@felesatra.moe>
2016-10-28 22:21:16 -0700

    Clean up fs with pathlib

--- mir/frelia/fs.py
+++ mir/frelia/fs.py
@@ -1,32 +1,27 @@
 """File system utilities."""
 
 import os
+import pathlib
 
 
 def find_files(path):
     """Yield the paths of all files in a directory tree."""
     for dirpath, dirnames, filenames in os.walk(path):
-        del dirnames
+        dirpath = pathlib.Path(dirpath)
         for filename in filenames:
-            yield os.path.join(dirpath, filename)
+            yield dirpath / filename
 
 
 def link_recursively(src_dir, dst_dir):
     """Hard link files recursively from src to dst."""
+    dst_dir = pathlib.Path(dst_dir)
     for filepath in find_files(src_dir):
-        rel_filepath = os.path.relpath(filepath, src_dir)
-        dst_filepath = os.path.join(dst_dir, rel_filepath)
-        link_parents(filepath, dst_filepath)
+        rel_filepath = filepath.relative_to(src_dir)
+        dst_filepath = dst_dir / rel_filepath
+        make_parents(dst_filepath)
+        os.link(filepath, dst_filepath)
 
 
-def link_parents(src, dst):
-    """Link src to dst, creating  parent directories."""
-    os.makedirs(os.path.dirname(dst), exist_ok=True)
-    os.link(src, dst)
-
-
-def write_parents(path, string):
-    """Write string to file at path, creating parent directories."""
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    with open(path, 'w') as file:
-        file.write(string)
+def make_parents(path):
+    """Make parent directories of path."""
+    pathlib.Path(path).parent.mkdir(parents=True, exist_ok=True)
