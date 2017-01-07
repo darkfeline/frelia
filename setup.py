d950ec30ca4cb0af19a49699cd0fcca4961f9ab8
Allen Li <darkfeline@felesatra.moe>
2017-01-06 22:06:19 -0800

    Update to 3.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     packages=['mir.frelia'],
